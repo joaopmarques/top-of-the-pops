6162f8e68cf646759f13daf7a0d4280ceccdd2c6
João P. Marques <joaop.marques@outlook.com>
2023-07-16 02:14:31 +0100

    feat: add SEO-friendly tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
